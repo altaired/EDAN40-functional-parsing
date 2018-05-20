50e63e82027b41d25a6511037d82a6d08496a9cc
Simon Persson <simon@akep.se>
2018-05-20 15:51:44 +0200

    Added types for Program/exec

--- Program.hs
+++ Program.hs
@@ -14,4 +14,5 @@ shw (Program stmts) = concatMap Statement.toString stmts
 prnt :: T -> IO ()
 prnt prgm = putStr $ shw prgm
 
+exec :: T -> [Integer] -> [Integer]
 exec (Program stmts) = Statement.exec stmts Dictionary.empty
